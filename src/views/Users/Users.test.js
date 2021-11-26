7e4f1a985cb9cb28cf17f19cc116c992e98b0a0b
Milosz Zajonc <milosz.zajonc@gmail.com>
2021-11-26 13:17:29 +0100

    [TST]: add unit test for rendering user data

--- src/views/Users/Users.test.js
+++ src/views/Users/Users.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Users from "./Users";
 
 it("should render a list of users", () => {
@@ -7,4 +8,14 @@ it("should render a list of users", () => {
   const exampleUser = screen.getByText(/Edward/i);
 
   expect(exampleUser).toBeInTheDocument();
+});
+
+it("should render a user data after redirect", () => {
+  render(<Users />);
+
+  const userLink = screen.getByText(/show/i);
+
+  userEvent.click(userLink);
+
+  expect(screen.getByText(/User Id:/i)).toBeInTheDocument();
 });
