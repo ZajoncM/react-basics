90d052cf590b0deba3e28bd7496aeecde1189b28
Milosz Zajonc <milosz.zajonc@gmail.com>
2021-11-26 11:50:12 +0100

    [TST]: Add unit test for submitting form

--- src/views/LoginForm/LoginForm.test.js
+++ src/views/LoginForm/LoginForm.test.js
@@ -1,5 +1,6 @@
 import LoginForm from "./LoginForm";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 it("should have a username, password and submit button", () => {
   render(<LoginForm />);
@@ -11,4 +12,22 @@ it("should have a username, password and submit button", () => {
   expect(usernameField).toBeInTheDocument();
   expect(passwordField).toBeInTheDocument();
   expect(submitButton).toBeInTheDocument();
+});
+
+it("should allow the user to submit their credentials", () => {
+  const submit = jest.fn();
+  render(<LoginForm submit={submit} />);
+
+  const usernameField = screen.getByLabelText(/username/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByText(/submit/i);
+
+  userEvent.type(usernameField, "Miłosz");
+  userEvent.type(passwordField, "secure");
+  userEvent.click(submitButton);
+
+  expect(submit).toHaveBeenCalledWith({
+    username: "Miłosz",
+    password: "secure",
+  });
 });
