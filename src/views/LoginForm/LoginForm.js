b74022753f4b4439de14b92f723feb98bc6b86ad
Milosz Zajonc <milosz.zajonc@gmail.com>
2021-11-26 11:43:20 +0100

    [FEAT]: Create LoginForm Component

--- src/views/LoginForm/LoginForm.js
+++ src/views/LoginForm/LoginForm.js
@@ -1,5 +1,14 @@
 const LoginForm = () => {
-  return "Implement me";
+  return (
+    <form>
+      <label htmlFor="username">Username: </label>
+      <input id="username" type="text" name="username" />
+      <label htmlFor="password">Password: </label>
+      <input id="password" type="password" name="password" />
+
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default LoginForm;
