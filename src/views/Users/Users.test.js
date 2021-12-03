364ed5caaf743dfcce26c378252a648a8399af1d
Milosz Zajonc <milosz.zajonc@gmail.com>
2021-12-03 11:29:40 +0100

    [NIT]: Modify tests

--- src/views/Users/Users.test.js
+++ src/views/Users/Users.test.js
@@ -1,18 +1,31 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./Users";
+import User from "../User/User";
 
 const MockUsers = () => {
   return (
     <BrowserRouter>
-      <Users />
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/users/:userId" element={<User />} />
+      </Routes>
     </BrowserRouter>
   );
 };
 
-it("should render a list of users", () => {
-  render(<MockUsers />);
-  const exampleUser = screen.getByText(/Show Edward/i);
+describe("Users", () => {
+  it("should render a list of users", () => {
+    render(<MockUsers />);
+    const exampleUser = screen.getByText(/Show Edward/i);
+    expect(exampleUser).toBeInTheDocument();
+  });
 
-  expect(exampleUser).toBeInTheDocument();
+  it("should redirect int user component", () => {
+    render(<MockUsers />);
+    const exampleUser = screen.getByText(/Show Edward/i);
+    fireEvent.click(exampleUser);
+
+    expect(screen.getByText("User: 4")).toBeInTheDocument();
+  });
 });
